1a48d1294ac9e6e0c0da6c2625713add6f351057
sachimugu <sachimugu@outlook.com>
2023-03-08 17:01:50 +0100

    setup and navbar

---
+++ components/header.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { ImFacebook, ImTwitter, ImYoutube } from "react-icons/im";
+
+export default function Header() {
+  return (
+    <header className="bg-gray-50">
+      <div className="xl:container xl:mx-auto flex flex-col sm:flex-row sm:justify-between text-center py-3">
+        <div className="md:flex-none w-96 order-2 sm:order-1 flex justify-center py-4 sm:py-0">
+          <input
+            className="input-text"
+            type="text"
+            placeholder="search..."
+          ></input>
+        </div>
+        <div className="w-96 sm:order-2">
+          <a className="font-bold uppercase text-3xl">Design</a>
+        </div>
+        <div className="w-96 order-3 flex justify-center">
+          <div className="flex gap-6">
+            <a>
+              <ImFacebook color="blue" />
+            </a>
+            <a>
+              <ImTwitter color="blue" />
+            </a>
+            <a>
+              <ImYoutube color="red" />
+            </a>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+}
