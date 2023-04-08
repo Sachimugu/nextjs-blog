836f889a1224d0d13916205653fc5fc9e74d2a6f
sachimugu <sachimugu@outlook.com>
2023-04-08 17:33:06 +0100

    header bg

--- components/header.js
+++ components/header.js
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import React from "react";
-import { ImFacebook, ImTwitter, ImLinkedin } from "react-icons/im";
+import {ImTwitter, ImLinkedin, ImGithub } from "react-icons/im";
 
 export default function Header() {
 
@@ -11,7 +11,7 @@ export default function Header() {
   };
 
   return (
-    <header className="bg-gray-50" style={bg}>
+    <header className="bg-gray-50">
       <div className="xl:container xl:mx-auto flex flex-col sm:flex-row sm:justify-between text-center py-3">
         <div className="md:flex-none w-96 order-2 sm:order-2 flex justify-center py-4 sm:py-0">
           <input
@@ -23,22 +23,25 @@ export default function Header() {
         <div className="w-96 sm:order-1">
           <Link
             href={"/"}
-            className="font-bold uppercase text-3xl hover:text-orange-500"
+            className="font-bold uppercase text-3xl text-orange-500  hover:text-orange-600"
           >
             Design
           </Link>
         </div>
         <div className="w-96 order-3 flex justify-center">
-          <div className="flex gap-6">
-            <a>
-              <ImFacebook color="blue" />
-            </a>
-            <a>
-              <ImTwitter color="blue" />
-            </a>
-            <a>
-              <ImLinkedin color="blue" />
-            </a>
+          <div className="flex gap-6  hover:text-orange-600">
+            <Link
+              className="hover:text-orange-600"
+              href="https://web.facebook.com/suleiman.achimugu/"
+            >
+              <ImGithub color="orange" />
+            </Link>
+            <Link href="https://www.linkedin.com/in/sachimugu/">
+              <ImLinkedin color="orange" />
+            </Link>
+            <Link href="https://twitter.com/achimugu_a">
+              <ImTwitter color="orange" />
+            </Link>
           </div>
         </div>
       </div>
